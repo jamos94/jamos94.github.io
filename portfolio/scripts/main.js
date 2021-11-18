676ff377508c817acf1bdafaf6a8db7824dc5baa
Jessica Amos <amosjessicar@gmail.com>
2021-11-17 20:52:40 -0500

    troubleshoot changing images

--- portfolio/scripts/main.js
+++ portfolio/scripts/main.js
@@ -2,9 +2,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/progressneverlinear') {
-      myImage.setAttribute('src','images/successinsteps');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/firefox2.png');
     } else {
-      myImage.setAttribute('src','images/progressneverlinear');
+      myImage.setAttribute('src','images/firefox-icon.png');
     }
 }
+
