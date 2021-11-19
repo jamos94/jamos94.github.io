f437e2e95dafd7d846cfd2a5a156648453e0068f
Jessica Amos <amosjessicar@gmail.com>
2021-11-19 10:24:38 -0500

    Adding  a button
    adding a button that updates headings with username. this name is stored in local storage

--- portfolio/scripts/main.js
+++ portfolio/scripts/main.js
@@ -1,3 +1,4 @@
+// image changes upon click
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -9,6 +10,24 @@ myImage.onclick = function() {
     }
 }
 
-// to do:
-    // image won't load in above function
-    //first image shows but second disapears on click instead of changing to the next image
+// creates a welcome message
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+//gets a user name, stores it, and updates heading
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+//allows the user to enter a new username
+myButton.onclick = function() {
+  setUserName();
+}
